37a85db4bce9f6013317261ffde965d2ecdcd5c3
Mikkelmattern <130869851+Mikkelmattern@users.noreply.github.com>
2025-12-19 13:52:30 +0100

    Lavet getMostExpensive() metode der returnerer en Sauna. Tilføjet metoder til run

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.io.FileNotFoundException;
 
 public class Main{
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         SaunaVillage s = new SaunaVillage();
         s.run();
 
--- src/SaunaVillage.java
+++ src/SaunaVillage.java
@@ -5,35 +5,38 @@ import java.util.Scanner;
 
 public class SaunaVillage {
     private final ArrayList<Sauna> saunas = new ArrayList<>();
+    private final Scanner sc = new Scanner(System.in);
 
-    private FileHandler fh = new FileHandler();
-    private Scanner sc = new Scanner(System.in);
-
-    public void run() throws FileNotFoundException {
+    public void run(){
         loadFromCSV("sauna");
         printReport();
         System.out.println("===Udskriv gennemsnit for===\n1:Wood\n2:Electric");
         int i = Integer.parseInt(sc.nextLine());
         double printAverage = 0;
         String type = "";
-        switch(i){
+        switch (i) {
             case 1 -> {
                 printAverage = getAverageCost("Wood");
                 type = "træ";
             }
-            case 2 ->{
+            case 2 -> {
                 printAverage = getAverageCost("electric");
                 type = "elektrisk";
             }
             default -> System.out.println("Vælg wood eller electric");
         }
         System.out.printf("Gennemsnitsomkostningerne for typen %s er %.2fkr", type, printAverage);
+        Sauna e = getMostExpensive();
+        Sauna c = getCheapest();
+        System.out.println("\n"+"\nDen dyrest at betjene er"+e.toString());
+        System.out.println("\nDen billigeste at betjene er"+c.toString());
+
     }
 
     public void printReport() {
         for (Sauna s : saunas) {
             double cost = s.calculateDailyCost();
-            System.out.println(cost+"kr");
+            System.out.println(cost + "kr");
         }
     }
 
@@ -55,18 +58,18 @@ public class SaunaVillage {
                 }
                 if (!(found == null)) {
                 }
-                    if (array[3].equalsIgnoreCase("wood")) {
-                        newWoodSauna = new WoodSauna(array[2]);
-                        found = newWoodSauna;
-                    } else {
-                        newElecricSauna = new ElectricSauna(array[2]);
-                        found = newElecricSauna;
-                    }
-                    found.addTemperature(Integer.parseInt(array[1]));
+                if (array[3].equalsIgnoreCase("wood")) {
+                    newWoodSauna = new WoodSauna(array[2]);
+                    found = newWoodSauna;
+                } else {
+                    newElecricSauna = new ElectricSauna(array[2]);
+                    found = newElecricSauna;
+                }
+                found.addTemperature(Integer.parseInt(array[1]));
                 saunas.add(found);
 
-                }
-            } catch (FileNotFoundException ex) {
+            }
+        } catch (FileNotFoundException ex) {
             throw new RuntimeException(ex);
         }
 
@@ -85,5 +88,41 @@ public class SaunaVillage {
             return 0;
         }
         return averageAmount / count;
+    }
+
+    private Sauna getMostExpensive() {
+        Sauna mostExpensive = null;
+        double prevExpensive = Double.NEGATIVE_INFINITY;
+        double i;
+        if(saunas.isEmpty()){
+            System.out.println("getMostExpensive er tom");
+            return null;
+        }
+        for (Sauna s : saunas) {
+             i = s.calculateDailyCost();
+            if (i > prevExpensive) {
+                mostExpensive = s;
+                prevExpensive = i;
+            }
+        }
+        return mostExpensive;
+    }
+
+    private Sauna getCheapest() {
+        Sauna cheapestSauna = null;
+        double prevCheapest = Double.POSITIVE_INFINITY;
+        double i;
+        if(saunas.isEmpty()){
+            System.out.println("getCheapest er tom");
+            return null;
+        }
+        for (Sauna s : saunas) {
+           i = s.calculateDailyCost();
+            if(prevCheapest>i){
+                prevCheapest = i;
+                cheapestSauna = s;
+            }
+        }
+        return cheapestSauna;
     }
 }
